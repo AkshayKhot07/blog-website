6a72b0dad8f2d01b1c477cd2e2d959bc152e2ff5
akzedexinfo07 <akshay.khot@zedexinfo.com>
2024-09-02 11:02:16 +0530

    vite config changes done

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/deploy_react_app_github_pages_vercel",
+  // base: process.env.VITE_BASE_PATH || "/deploy_react_app_github_pages_vercel",
+  base: process.env.VITE_BASE_PATH || "/",
 });
