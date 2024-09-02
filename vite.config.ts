2ced687a1e817fd6cc8fcd57df3309ac4792a8a3
akzedexinfo07 <akshay.khot@zedexinfo.com>
2024-09-02 10:13:32 +0530

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/AkshayKhot07/blog-website",
+  base: "/blog-website",
   plugins: [react()],
 });
