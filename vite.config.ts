218d737dabc1a2303d07e98dd974608f12a50694
akzedexinfo07 <akshay.khot@zedexinfo.com>
2024-09-02 10:06:57 +0530

    github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/AkshayKhot07/blog-website",
   plugins: [react()],
-})
+});
